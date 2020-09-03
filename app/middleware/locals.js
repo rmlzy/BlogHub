702f09ae6639b6e84f033e60f8c00fe523e96a80
rmlzy <rmlzy@outlook.com>
2020-09-03 17:12:03 +0800

    feat: update default theme

--- app/middleware/locals.js
+++ app/middleware/locals.js
@@ -3,11 +3,6 @@
 module.exports = () => {
   return async function (ctx, next) {
     ctx.locals.version = ctx.app.config.version;
-    if (ctx.request.headers.cookie) {
-      ctx.locals.theme = ctx.request.headers.cookie.includes("theme=dark") ? "dark" : "light";
-    } else {
-      ctx.locals.theme = "light";
-    }
     await next();
   };
 };
--- app/public/js/theme.js
+++ app/public/js/theme.js
@@ -1,38 +1,8 @@
-function setCookie(name, value, days) {
-  var expires = "";
-  if (days) {
-    var date = new Date();
-    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-    expires = "; expires=" + date.toUTCString();
-  }
-  document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
-
-function getCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(";");
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == " ") c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-  }
-  return null;
-}
-
 function setHljsCss(isDark) {
   var cssName = isDark ? "darcula.css" : "github.css";
   var cssUrl = "/public/lib/highlight/styles/" + cssName;
   var styleEl = document.getElementById("js_hljsCss");
   styleEl.setAttribute("href", cssUrl);
-  setCookie("theme", isDark ? "dark" : "light", 10);
-
-  if (document.body) {
-    if (isDark) {
-      document.body.className = "bloghub theme-dark";
-    } else {
-      document.body.className = "bloghub theme-light";
-    }
-  }
 }
 
 function initAndDeleteOsTheme() {
